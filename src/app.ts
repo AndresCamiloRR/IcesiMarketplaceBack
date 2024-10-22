3a510acf32f2c6dac22ca0dc72bcdea7549c0900
AndresCamiloRR <101947895+AndresCamiloRR@users.noreply.github.com>
2024-10-22 17:47:20 -0500

    CORS

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function createApp() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    });
     return app;
 }
 
